6fbca93e2fb4a776095915225792089d703e4c47
chaitanyakherdikar-hub <chaitanyakherdikar@gmail.com>
2025-10-02 21:55:25 +0530

    name changed

--- my-project/src/components/Hero.jsx
+++ my-project/src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
       style={{ backgroundImage: "url('/ecommerceweb/Hero.jpg')" }}
     >
       <div className="bg-black bg-opacity-70 p-8 rounded-xl text-center">
-        <h1 className="text-5xl font-bold mb-4 text-purple-400">StreetoGraphs</h1>
+        <h1 className="text-5xl font-bold mb-4 text-purple-400">Chaitanya</h1>
         <p className="text-lg text-gray-300">
           Capturing the raw beauty of the streets — one shot at a time.
         </p>
